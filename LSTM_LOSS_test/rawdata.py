cf879d0642e8ac77be3d2dcdc30c2e672d9009c1
flag625 <liangzhisen@outlook.com>
2018-08-18 21:23:55 +0800

    获取股票历史数据

--- LSTM_LOSS_test/rawdata.py
+++ LSTM_LOSS_test/rawdata.py
@@ -3,28 +3,23 @@
 import tushare as ts
 import pandas as pd
 from CommonAPI.base import Base
-from CommonAPI import loggingData
 
-logger = loggingData()
 
 def stockhistory(code_list, start_date, end_date, conns):
     base = Base()
     financial_data = conns['financial_data']
-    fin = []
+    res = pd.DataFrame()
     for code in code_list:
         df = ts.get_k_data(code, start=start_date, end=end_date, autype=None)
         if df.empty:
             continue
-        fin.append(df)
-        print(fin)
+        res = res.append(df, ignore_index=True)
 
-    res = pd.DataFrame(fin)
     print(res)
 
 if __name__ == "__main__":
-     # base = Base()
-     # financial_data = base.conn('financial_data')
-     # conns = {'financial_data': financial_data}
-     # code_list = ['000001']
-     # stockhistory(code_list, '2018-08-01', '2018-08-17', conns)
-     print("..")
+     base = Base()
+     financial_data = base.conn('financial_data')
+     conns = {'financial_data': financial_data}
+     code_list = ['000001']
+     stockhistory(code_list, '2017-07-01', '2018-07-31', conns)
