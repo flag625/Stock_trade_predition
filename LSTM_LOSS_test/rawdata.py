7e946436ff66a7e83f65d1d8d1f02069c7d11d2f
flag625 <liangzhisen@outlook.com>
2018-08-22 16:50:15 +0800

    收集股票数据，存储在mysql

--- LSTM_LOSS_test/rawdata.py
+++ LSTM_LOSS_test/rawdata.py
@@ -8,19 +8,23 @@ from CommonAPI.base import Base
 def stockhistory(code_list, start_date, end_date, conns):
     base = Base()
     financial_data = conns['financial_data']
-    res = pd.DataFrame()
+    # res = pd.DataFrame()
     for code in code_list:
+        res = pd.DataFrame()
         df = ts.get_k_data(code, start=start_date, end=end_date, autype=None)
         if df.empty:
             continue
         res = res.append(df, ignore_index=True)
+        table = 'stock-'+code
+        # print(table)
+        base.batchwri(res, table, financial_data)
 
-    print(res)
-    base.batchwri(res, 'stock-000001', financial_data)
+    # print(res)
+    # base.batchwri(res, 'stock-000001', financial_data)
 
 if __name__ == "__main__":
     base = Base()
     financial_data = base.conn('financial_data')
     conns = {'financial_data': financial_data}
-    code_list = ['000001']
+    code_list = ['000004','000005']
     stockhistory(code_list, '2017-07-01', '2018-07-31', conns)
