53db41564a32b5a182f86a3e7bc764397fd6d717
flag625 <liangzhisen@outlook.com>
2018-08-22 16:41:02 +0800

    数据库常用函数

--- CommonAPI/DBcommon.py
+++ CommonAPI/DBcommon.py
@@ -103,7 +103,7 @@ class mysql2pd(object):
         try:
             engine = create_engine("mysql+pymysql://"+self.user+":"+self.pwd+"@"+
                                    self.host+":"+self.port+"/"+self.db+"?charset=utf8")
-            dataframe.to_sql(name=table, con=engine, if_exists='qppend', index=False, index_label=False)
+            dataframe.to_sql(name=table, con=engine, if_exists='append', index=False, index_label=False)
             res = True
         except Exception as e:
             print(e)
